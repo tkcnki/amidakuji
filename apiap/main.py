c4407282b82abc684c4ac76a16beff3f0236855f
tkcnki <github@luna2.info>
2021-01-13 15:19:49 +0000

    little update

--- apiap/main.py
+++ apiap/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
+from random import shuffle
+import time
 
 app = FastAPI()
 
@@ -8,15 +10,30 @@ class Param(BaseModel):
     Names: List[str] = []
     NumberInGroup: int
 
+Group_name_list = ["Alice", "Bob", "Carol", "Eve", "Peggy"]
+
 @app.get("/amida/{key}")
 async def amida(key, param: Param):
 
+    # Key check
     if key != "hJqDzsXImQOkU0fgiVnX":
+        time.sleep(3)
         return {"message": "Hello World"}
+
+    len_names = len(param.Names)
+
+    if len_names < Param.NumberInGroup:
+        return {"message": "Ooops"}
 
     class Result(BaseModel):
         GroupName: str
         GroupMembers: List[str] = []
+
+    if Param.NumberInGroup < 3 and 3 <= len_names <= 4 :
+        return Result(GroupName = Group_name_list[0], GroupMembers = param.Names)
+
+    shuffle(param.Names)
+
 
     result = Result(GroupName= "Group-1", GroupMembers= param.Names[0:4])
 
